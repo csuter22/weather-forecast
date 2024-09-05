08acba8fa969e5de9d30a582d5466d67d4d8d7d3
camila <camisuter@gmail.com>
2024-09-05 12:37:01 +0300

    inject forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -56,3 +56,27 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+
+function displayForecast() {
+
+let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+let forecastHtml = "";
+
+days.forEach(function (day) {
+forecastHtml = forecastHtml + ` <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤</div>
+        <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+            <strong>15°</strong> 9°</div>
+            </div>
+        </div>`;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml
+
+}
+
+displayForecast();
